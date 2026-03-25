2c1d1b3b28b4e91780d6ed27bed916453adc6cdb
João Prata <jprata@agap2.pt>
2026-03-25 15:48:09 +0000

    CH5 - L3

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"log"
+	"os"
 	"net/http"
 	"sync/atomic"
+	"database/sql"
+	"github.com/joho/godotenv"
+	"github.com/JoaoPrata/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error opening db")
+	}
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries: database.New(db),
 	}
 
 	mux := http.NewServeMux()
