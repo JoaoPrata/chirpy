0d9c5eacaa00ac8ebdcce61b473fbb3a7f746c20
João Prata <jprata@agap2.pt>
2026-04-08 09:02:09 +0100

    CH7 - L4

--- handler_chirps.go
+++ handler_chirps.go
@@ -72,7 +72,7 @@ func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	data, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could't get chirps", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps", err)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, mapChirps(data))
@@ -81,15 +81,50 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
 	id, err := uuid.Parse(r.PathValue("id"))
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Could't parse id", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse id", err)
 		return
 	}
 	data, err := cfg.db.GetChirp(r.Context(), id)
 	if err != nil {
-		respondWithJSON(w, http.StatusNotFound, "Could't get chirp")
+		respondWithJSON(w, http.StatusNotFound, "Couldn't get chirp")
 		return
 	}
 	respondWithJSON(w, http.StatusOK, mapChirp(data))
+}
+
+func (cfg *apiConfig) handlerDeleteChirp(w http.ResponseWriter, r *http.Request) {
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't get token", err)
+		return
+	}
+	userID, err := auth.ValidateJWT(token, cfg.tokenSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Invalid token", err)
+		return
+	}
+
+	id, err := uuid.Parse(r.PathValue("id"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse id", err)
+		return
+	}
+	chirp, err := cfg.db.GetChirp(r.Context(), id)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find chirp", err)
+		return
+	}
+	if chirp.UserID != userID {
+		respondWithJSON(w, http.StatusForbidden, "Chirp dosen't belong to user")
+		return
+	}
+
+	err = cfg.db.DeleteChirp(r.Context(), id)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't delete chirp", err)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func validateChirp(body string) (string, error) {
